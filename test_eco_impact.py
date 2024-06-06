a7bf71f97cbd6eaeb400e9f3ddf99869760a2d87
Denis Labunin <denis.labunin@mail.ru>
2024-06-06 11:59:03 +0300

    minor improvements

--- test_eco_impact.py
+++ test_eco_impact.py
@@ -8,17 +8,17 @@ class TestEcoImpactCounter(BaseTest):
     def test_tc01(self, browser):
         self.eco_impact_page.open(LINK)
         self.eco_impact_page.is_opened(LINK)
-        self.eco_impact_page.is_energy_counter_displayed()
+        assert self.eco_impact_page.is_energy_counter_displayed(), 'Элемент не найден'
         self.eco_impact_page.energy_take_screenshot()
 
     def test_tc02(self, browser):
         self.eco_impact_page.open(LINK)
         self.eco_impact_page.is_opened(LINK)
-        self.eco_impact_page.is_water_counter_displayed()
+        assert self.eco_impact_page.is_water_counter_displayed(), 'Элемент не найден'
         self.eco_impact_page.water_take_screenshot()
 
     def test_tc03(self, browser):
         self.eco_impact_page.open(LINK)
         self.eco_impact_page.is_opened(LINK)
-        self.eco_impact_page.is_co2_counter_displayed()
+        assert self.eco_impact_page.is_co2_counter_displayed(), 'Элемент не найден'
         self.eco_impact_page.co2_take_screenshot()
